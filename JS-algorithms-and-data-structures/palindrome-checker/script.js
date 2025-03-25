5fec0b762aba9b490e78486a97389ab55d2c6fc7
unknown <xopox2015@gmail.com>
2025-03-25 16:30:59 +0100

    formatting

--- JS-algorithms-and-data-structures/palindrome-checker/script.js
+++ JS-algorithms-and-data-structures/palindrome-checker/script.js
@@ -5,31 +5,31 @@ const inputResult = document.getElementById("result");
 buttonCheck.addEventListener("click", addTextToDiv);
 
 function addTextToDiv() {
-	if (originalInput.value == "") {
-		alert("Please input a value");
-	} else {
-		inputResult.innerHTML = checkPalindrome(originalInput.value);
-	}
+    if (originalInput.value == "") {
+        alert("Please input a value");
+    } else {
+        inputResult.innerHTML = checkPalindrome(originalInput.value);
+    }
 }
 
 function checkPalindrome(text) {
-	const stringFirst = text.replace(/[^A-Za-z0-9]/g, "");
-	const stringLowerCase = stringFirst.toLowerCase();
-	const arrFirst = stringLowerCase.split("");
-	const arrSecond = arrFirst.toReversed();
+    const stringFirst = text.replace(/[^A-Za-z0-9]/g, "");
+    const stringLowerCase = stringFirst.toLowerCase();
+    const arrFirst = stringLowerCase.split("");
+    const arrSecond = arrFirst.toReversed();
 
-	if (compareTwoArrays(arrFirst, arrSecond)) {
-		return `${text} is a palindrome`;
-	} else {
-		return `${text} is not a palindrome`;
-	}
+    if (compareTwoArrays(arrFirst, arrSecond)) {
+        return `${text} is a palindrome`;
+    } else {
+        return `${text} is not a palindrome`;
+    }
 }
 
 function compareTwoArrays(arrFirst, arrSecond) {
-	for (let i = 0; i < arrFirst.length; i++) {
-		if (arrFirst[i] !== arrSecond[i]) {
-			return false;
-		}
-	}
-	return true;
+    for (let i = 0; i < arrFirst.length; i++) {
+        if (arrFirst[i] !== arrSecond[i]) {
+            return false;
+        }
+    }
+    return true;
 }
