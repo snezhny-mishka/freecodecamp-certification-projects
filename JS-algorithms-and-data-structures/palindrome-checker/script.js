6e4bd4bd0f7a2bf1fd8b8b48a3572b22efdc833a
unknown <xopox2015@gmail.com>
2025-03-25 11:47:17 +0100

    reformatted the code, deleted the unnecessary comments

--- JS-algorithms-and-data-structures/palindrome-checker/script.js
+++ JS-algorithms-and-data-structures/palindrome-checker/script.js
@@ -1,46 +1,30 @@
-
-
-
 const originalInput = document.getElementById("text-input");
-// const originalInput = "eyes";
 const buttonCheck = document.getElementById("check-btn");
 const inputResult = document.getElementById("result");
-// const regex = /[^A-Za-z0-9]/;  //all the characters we don't need
 
-// if (originalInput.value =='') {
-// 	alert('Please input a value');
-// 	return;
-// }
-
-buttonCheck.addEventListener('click', addTextToDiv);
+buttonCheck.addEventListener("click", addTextToDiv);
 
 function addTextToDiv() {
-	if (originalInput.value == '') {
+	if (originalInput.value == "") {
 		alert("Please input a value");
+	} else {
+		inputResult.innerHTML = checkPalindrome(originalInput.value);
 	}
-	else {
-	inputResult.innerHTML = checkPalindrome(originalInput.value);
-	}
-} 
+}
 
 function checkPalindrome(text) {
-
-	const stringFirst = text.replace(/[^A-Za-z0-9]/g, ''); 
+	const stringFirst = text.replace(/[^A-Za-z0-9]/g, "");
 	const stringLowerCase = stringFirst.toLowerCase();
-
-	//turn my String to Array
-	const arrFirst = stringLowerCase.split('');
-	//Now I have the 1st array to compare and need to create a reversed copy
+	const arrFirst = stringLowerCase.split("");
 	const arrSecond = arrFirst.toReversed();
-	
-	if(compareTwoArrays(arrFirst, arrSecond)) {
+
+	if (compareTwoArrays(arrFirst, arrSecond)) {
 		return `${text} is a palindrome`;
-	}
-	else {
+	} else {
 		return `${text} is not a palindrome`;
 	}
 }
-	//now I have to compare the two arrays
+
 function compareTwoArrays(arrFirst, arrSecond) {
 	for (let i = 0; i < arrFirst.length; i++) {
 		if (arrFirst[i] !== arrSecond[i]) {
@@ -49,15 +33,3 @@ function compareTwoArrays(arrFirst, arrSecond) {
 	}
 	return true;
 }
-
-
-
-
-
-
-
-
-
-
-
-
